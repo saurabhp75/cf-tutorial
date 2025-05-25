9f71f92362e1e4b9b3186f25b8baa1642bbc8ac2
Saurabh Prakash <saurabhp75@gmail.com>
2025-05-25 22:43:23 +0530

    Add tests

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,24 +1,51 @@
-import { env, createExecutionContext, waitOnExecutionContext, SELF } from 'cloudflare:test';
+import { SELF } from 'cloudflare:test';
 import { describe, it, expect } from 'vitest';
-import worker from '../src/index';
 
-// For now, you'll need to do something like this to get a correctly-typed
-// `Request` to pass to `worker.fetch()`.
-const IncomingRequest = Request<unknown, IncomingRequestCfProperties>;
-
-describe('Hello World worker', () => {
-	it('responds with Hello World! (unit style)', async () => {
-		const request = new IncomingRequest('http://example.com');
-		// Create an empty context to pass to `worker.fetch()`.
-		const ctx = createExecutionContext();
-		const response = await worker.fetch(request, env, ctx);
-		// Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
-		await waitOnExecutionContext(ctx);
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
+describe('Photo service', () => {
+	it('returns a 404 if a non-existent endpoint is called', async () => {
+		const response = await SELF.fetch('http://localhost:8787/2');
+		expect(response.status).toEqual(404);
 	});
+});
 
-	it('responds with Hello World! (integration style)', async () => {
-		const response = await SELF.fetch('https://example.com');
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
+describe('GET /images', () => {
+	// Test for status code
+	it('returns a 200 OK response', async () => {
+		const response = await SELF.fetch('http://localhost:8787/images');
+		expect(response.status).toEqual(200);
+	});
+	// Test for response content
+	it('should return images in the response', async () => {
+		const response = await SELF.fetch('http://localhost:8787/images');
+		const json = await response.json();
+		expect(json).toEqual(expect.arrayContaining([expect.objectContaining({ id: 3, url: 'https://bar.com/img1', author: 'Lara Lobster' })]));
+	});
+});
+
+describe('POST /images', () => {
+	it('should return a 201 response code', async () => {
+		const payload = {
+			id: 4,
+			url: 'https://example.com/some_image.png',
+			author: 'Lia',
+		};
+		const response = await SELF.fetch('http://localhost:8787/images', {
+			method: 'POST',
+			body: JSON.stringify(payload),
+		});
+		expect(response.status).toEqual(201);
+	});
+	it('should return the created image in the response', async () => {
+		const newImage = {
+			id: 4,
+			url: 'https://example.com/some_image.png',
+			author: 'Lia',
+		};
+		const response = await SELF.fetch('http://localhost:8787/images', {
+			method: 'POST',
+			body: JSON.stringify(newImage),
+		});
+		const json = await response.json();
+		expect(json).toEqual(expect.objectContaining(newImage));
 	});
 });
